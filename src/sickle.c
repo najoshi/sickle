092ff144654334319b92fc1188aed30b396ff769
Nikhil Joshi <najoshi@ucdavis.edu>
2011-02-09 01:45:45 -0800

    refactored code so that there is only one executable with pe and se commands

---
+++ src/sickle.c
@@ -0,0 +1,54 @@
+#include <assert.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <zlib.h>
+#include <stdio.h>
+#include <getopt.h>
+#include <string.h>
+#include "sickle.h"
+
+void main_usage (int status) {
+
+	fprintf (stdout, "\nUsage: %s <command> [options]\n\
+\n\
+Command:\n\
+pe\tpaired-end sequence trimming\n\
+se\tsingle-end sequence trimming\n\
+\n\
+--help, display this help and exit\n\
+--version, output version information and exit\n\n", PROGRAM_NAME);
+
+	exit (status);
+}
+
+int main (int argc, char *argv[]) {
+	int retval=0;
+
+	if (argc < 2 || (strcmp (argv[1],"pe") != 0 && strcmp (argv[1],"se") != 0 && strcmp (argv[1],"--version") != 0 && strcmp (argv[1],"--help") != 0)) {
+		main_usage (EXIT_FAILURE);
+	}
+
+	if (strcmp (argv[1],"--version") == 0) {
+		fprintf(stdout, "%s version %0.3f\nCopyright (c) 2011 The Regents of University of California, Davis Campus.\n%s is free software and comes with ABSOLUTELY NO WARRANTY.\nDistributed under the MIT License.\n\nWritten by %s\n", PROGRAM_NAME, VERSION, PROGRAM_NAME, AUTHORS);
+
+		exit (EXIT_SUCCESS);
+
+	}
+
+	else if (strcmp (argv[1],"--help") == 0) {
+		main_usage (EXIT_SUCCESS);
+	}
+
+	else if (strcmp (argv[1],"pe") == 0) {
+		retval = paired_main (argc, argv);
+		return (retval);
+	}
+
+	else if (strcmp (argv[1],"se") == 0) {
+		retval = single_main (argc, argv);
+		return (retval);
+	}
+
+	return 0;
+}
--- src/trim_paired.c
+++ src/trim_paired.c
@@ -1,62 +1,18 @@
 #include <assert.h>
 #include <ctype.h>
 #include <stdlib.h>
-#include <limits.h>
 #include <zlib.h>
 #include <stdio.h>
 #include "kseq.h"
 #include <getopt.h>
+#include "sickle.h"
 
 KSEQ_INIT(gzFile, gzread)
 
-#ifndef PROGRAM_NAME
-#define PROGRAM_NAME "trim_paired"
-#endif
+int paired_qual_threshold = 20;
+int paired_length_threshold = 20;
 
-#ifndef AUTHORS
-#define AUTHORS "Nikhil Joshi, UC Davis Bioinformatics Core\n"
-#endif
-
-#ifndef VERSION
-#define VERSION 0.0
-#endif
-
-#define ILLUMINA_TYPE 1
-#define PHRED_TYPE 2
-#define SANGER_TYPE 3
-
-
-/* Options drawn from GNU's coreutils/src/system.h */
-/* These options are defined so as to avoid conflicting with option
-values used by commands */
-enum {
-  GETOPT_HELP_CHAR = (CHAR_MIN - 2),
-  GETOPT_VERSION_CHAR = (CHAR_MIN - 3)
-};
-#define GETOPT_HELP_OPTION_DECL \
-"help", no_argument, NULL, GETOPT_HELP_CHAR
-#define GETOPT_VERSION_OPTION_DECL \
-"version", no_argument, NULL, GETOPT_VERSION_CHAR
-#define case_GETOPT_HELP_CHAR \
-case GETOPT_HELP_CHAR: \
-usage(EXIT_SUCCESS); \
-break;
-#define case_GETOPT_VERSION_CHAR(Program_name, Version, Authors) \
-case GETOPT_VERSION_CHAR: \
-fprintf(stdout, "%s version %0.3f\nCopyright (c) 2011 The Regents " \
-"of University of California, Davis Campus.\n" \
-"%s is free software and comes with ABSOLUTELY NO WARRANTY.\n"\
-"Distributed under the MIT License.\n\nWritten by %s\n", \
-Program_name, Version, Program_name, Authors); \
-exit(EXIT_SUCCESS); \
-break;
-/* end code drawn from system.h */
-
-
-int qual_threshold = 20;
-int length_threshold = 20;
-
-static struct option long_options[] = {
+static struct option paired_long_options[] = {
 	{"pe-file1", required_argument, 0, 'f'},
 	{"pe-file2", required_argument, 0, 'r'},
 	{"qual-type", required_argument, 0, 't'},
@@ -70,32 +26,35 @@ static struct option long_options[] = {
 	{NULL, 0, NULL, 0}
 };
 
-void usage (int status) {
+void paired_usage (int status) {
 
-	fprintf (stdout, "\nUsage: %s -f <paired-end fastq file 1> -r <paired-end fastq file 2> -t <quality type> -o <trimmed pe file 1> -p <trimmed pe file 2> -s <trimmed singles file>\n\
+	fprintf (stderr, "\nUsage: %s pe -f <paired-end fastq file 1> -r <paired-end fastq file 2> -t <quality type> -o <trimmed pe file 1> -p <trimmed pe file 2> -s <trimmed singles file>\n\
 \n\
 Options:\n\
 -f, --pe-file1, Input paired-end fastq file 1 (required, must have same number of records as pe2)\n\
 -r, --pe-file2, Input paired-end fastq file 2 (required, must have same number of records as pe1)\n\
 -t, --qual-type, Type of quality values (illumina, phred, sanger) (required)\n\
--o, --output-pe1, Output trimmed fastq file 1 (required)\n\
--p, --output-pe2, Output trimmed fastq file 2 (required)\n\
+-o, --output-pe1, Output trimmed fastq file 1 (required)\n", PROGRAM_NAME);
+
+	fprintf (stderr, "-p, --output-pe2, Output trimmed fastq file 2 (required)\n\
 -s, --output-single, Output trimmed singles fastq file (required)\n\
 -q, --qual-threshold, Threshold for trimming based on average quality in a window. Default 20.\n\
 -l, --length-threshold, Threshold to keep a read based on length after trimming. Default 20.\n\
 --help, display this help and exit\n\
---version, output version information and exit\n\n", PROGRAM_NAME);
+--version, output version information and exit\n\n");
 
 	exit (status);
 }
 
-int get_quality_num (char qualchar, int qualtype) {
+int paired_get_quality_num (char qualchar, int qualtype) {
 	if (qualtype == ILLUMINA_TYPE) return ((int) qualchar - 64);
 	else if (qualtype == PHRED_TYPE) return ((int) qualchar);
 	else if (qualtype == SANGER_TYPE) return ((int) qualchar - 33);
+
+	return 0;
 }
 
-int main (int argc, char *argv[]) {
+int paired_main (int argc, char *argv[]) {
 
 	gzFile pe1=NULL;
 	gzFile pe2=NULL;
@@ -109,15 +68,16 @@ int main (int argc, char *argv[]) {
 	int optc;
 	extern char *optarg;
 	int qualtype=-1;
+	int window_size,i,j,window_start,window_total,p1flag,p2flag,p1cut,p2cut;
 
 	while (1) {
 		int option_index = 0;
-		optc = getopt_long (argc, argv, "df:r:t:o:p:s:q:l:", long_options, &option_index);
+		optc = getopt_long (argc, argv, "df:r:t:o:p:s:q:l:", paired_long_options, &option_index);
 
 		if (optc == -1) break;
 
 		switch (optc) {
-			if (long_options[option_index].flag != 0) break;
+			if (paired_long_options[option_index].flag != 0) break;
 
 			case 'f':
 				pe1 = gzopen (optarg, "r");
@@ -170,16 +130,16 @@ int main (int argc, char *argv[]) {
 				break;
 
 			case 'q':
-				qual_threshold = atoi (optarg);
-				if (qual_threshold < 0) {
+				paired_qual_threshold = atoi (optarg);
+				if (paired_qual_threshold < 0) {
 					fprintf (stderr, "Quality threshold must be >= 0\n");
 					return EXIT_FAILURE;
 				}
 				break;
 
 			case 'l':
-				length_threshold = atoi (optarg);
-				if (length_threshold < 0) {
+				paired_length_threshold = atoi (optarg);
+				if (paired_length_threshold < 0) {
 					fprintf (stderr, "Length threshold must be >= 0\n");
 					return EXIT_FAILURE;
 				}
@@ -189,29 +149,27 @@ int main (int argc, char *argv[]) {
 				debug = 1;
 				break;
 
-			case_GETOPT_HELP_CHAR;
+			case_GETOPT_HELP_CHAR(paired_usage);
 			case_GETOPT_VERSION_CHAR(PROGRAM_NAME, VERSION, AUTHORS);
 
 			case '?':
-				usage (EXIT_FAILURE);
+				paired_usage (EXIT_FAILURE);
 				break;
 
 			default:
-				usage (EXIT_FAILURE);
+				paired_usage (EXIT_FAILURE);
 				break;
 		}
 	}
 
 
 	if (!pe1 || !pe2 || !outfile1 || !outfile2 || !single || qualtype == -1) {
-		usage (EXIT_FAILURE);
+		paired_usage (EXIT_FAILURE);
 	}
 
 
 	fqrec1 = kseq_init (pe1);
 	fqrec2 = kseq_init (pe2);
-
-	int window_size,i,j,window_start,window_total,p1flag,p2flag,p1cut,p2cut;
 
 	while ((l1 = kseq_read (fqrec1)) >= 0) {
 
@@ -232,7 +190,7 @@ int main (int argc, char *argv[]) {
 		if (window_size == 0) window_size = fqrec1->seq.l;
 
 		for (i=0; i<window_size; i++) {
-			window_total += get_quality_num (fqrec1->qual.s[i], qualtype);
+			window_total += paired_get_quality_num (fqrec1->qual.s[i], qualtype);
 		}
 if (debug) printf ("window total: %d, window_size: %d\n", window_total, window_size);
 
@@ -242,14 +200,14 @@ if (debug) printf ("window total / window size: %f\n", (double)window_total / (d
 
 			/* if the average quality in the window is less than the threshold */
 			/* or if the window is the last window in the read */
-			if (((double)window_total / (double)window_size < qual_threshold) || 
+			if (((double)window_total / (double)window_size < paired_qual_threshold) || 
 				(window_start+window_size > fqrec1->qual.l)) {
 
 				/* at what point in the window does the quality dip below the threshold? */
 				for (j=window_start; j<window_start+window_size; j++) {
-					if (get_quality_num (fqrec1->qual.s[j], qualtype) < qual_threshold) {
+					if (paired_get_quality_num (fqrec1->qual.s[j], qualtype) < paired_qual_threshold) {
 						p1cut = j;
-						if (p1cut < length_threshold) {p1flag = 0;}
+						if (p1cut < paired_length_threshold) {p1flag = 0;}
 						break;
 					}
 				}
@@ -258,8 +216,8 @@ if (debug) printf ("window total / window size: %f\n", (double)window_total / (d
 			}
 
 			/* instead of sliding the window, subtract the first qual and add the next qual */
-			window_total -= get_quality_num (fqrec1->qual.s[window_start], qualtype);
-			window_total += get_quality_num (fqrec1->qual.s[window_start+window_size], qualtype);
+			window_total -= paired_get_quality_num (fqrec1->qual.s[window_start], qualtype);
+			window_total += paired_get_quality_num (fqrec1->qual.s[window_start+window_size], qualtype);
 			window_start++;
 
 if (debug) printf ("window total: %d\n", window_total);
@@ -277,7 +235,7 @@ if (debug) printf ("window total: %d\n", window_total);
 		if (window_size == 0) window_size = fqrec2->seq.l;
 
 		for (i=0; i<window_size; i++) {
-			window_total += get_quality_num (fqrec2->qual.s[i], qualtype);
+			window_total += paired_get_quality_num (fqrec2->qual.s[i], qualtype);
 		}
 if (debug) printf ("window total: %d, window_size: %d\n", window_total, window_size);
 
@@ -287,14 +245,14 @@ if (debug) printf ("window total / window size: %f\n", (double)window_total / (d
 
 			/* if the average quality in the window is less than the threshold */
 			/* or if the window is the last window in the read */
-			if (((double)window_total / (double)window_size < qual_threshold) || 
+			if (((double)window_total / (double)window_size < paired_qual_threshold) || 
 				(window_start+window_size > fqrec2->qual.l)) {
 
 				/* at what point in the window does the quality dip below the threshold? */
 				for (j=window_start; j<window_start+window_size; j++) {
-					if (get_quality_num (fqrec2->qual.s[j], qualtype) < qual_threshold) {
+					if (paired_get_quality_num (fqrec2->qual.s[j], qualtype) < paired_qual_threshold) {
 						p2cut = j;
-						if (p2cut < length_threshold) {p2flag = 0;}
+						if (p2cut < paired_length_threshold) {p2flag = 0;}
 						break;
 					}
 				}
@@ -303,8 +261,8 @@ if (debug) printf ("window total / window size: %f\n", (double)window_total / (d
 			}
 
 			/* instead of sliding the window, subtract the first qual and add the next qual */
-			window_total -= get_quality_num (fqrec2->qual.s[window_start], qualtype);
-			window_total += get_quality_num (fqrec2->qual.s[window_start+window_size], qualtype);
+			window_total -= paired_get_quality_num (fqrec2->qual.s[window_start], qualtype);
+			window_total += paired_get_quality_num (fqrec2->qual.s[window_start+window_size], qualtype);
 			window_start++;
 
 if (debug) printf ("window total: %d\n", window_total);
