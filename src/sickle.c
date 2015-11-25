3b79259ca561d7bd997476fc496dbe738f1c49ae
Matt Settles <mattsettles@gmail.com>
2015-11-25 15:06:06 -0800

    fixed main and segmentation fault

--- src/sickle.c
+++ src/sickle.c
@@ -25,17 +25,24 @@ se\tsingle-end sequence trimming\n\
 int main (int argc, char *argv[]) {
 	int retval=0;
 
+	if (argc < 2 || (strcmp (argv[1],"pe") != 0 && strcmp (argv[1],"se") != 0 && strcmp (argv[1],"--version") != 0 && strcmp (argv[1],"--help") != 0)) {
+		main_usage (EXIT_FAILURE);
+	}
+
 	if (strcmp (argv[1],"--version") == 0) {
 		fprintf(stderr, "%s version %0.2f\nCopyright (c) 2011 The Regents of University of California, Davis Campus.\n%s is free software and comes with ABSOLUTELY NO WARRANTY.\nDistributed under the MIT License.\n\nWritten by %s\n", PROGRAM_NAME, VERSION, PROGRAM_NAME, AUTHORS);
-
 		exit (EXIT_SUCCESS);
-
-	}
-
-	else if (strcmp (argv[1],"--help") == 0) {
+	} else if (strcmp (argv[1],"--help") == 0) {
 		main_usage (EXIT_SUCCESS);
+	} else if (strcmp (argv[1],"pe") == 0) {
+		retval = paired_main (argc, argv);
+		return (retval);
+	} else if (strcmp (argv[1],"se") == 0) {
+		retval = single_main (argc, argv);
+		return (retval);
+	} else {
+		main_usage (EXIT_FAILURE);
 	}
 
-	retval = paired_main (argc, argv);
-	return (retval);
+	return 0;
 }
